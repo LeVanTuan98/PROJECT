0bc060e4fdc853427e9ac869dd215d3675dc268d
levantuan98 <tuan.lv164352@sis.hust.edu.vn>
2019-05-31 18:36:55 +0700

    31/5/19

---
+++ js/responsive.js
@@ -0,0 +1,10 @@
+function Show() {
+    document.getElementById('menu-responsive').style.display = 'block';
+    document.getElementById('btn').style.display = 'none';
+    document.getElementById('btn-x').style.display = 'block';
+}
+function Hide() {
+    document.getElementById('menu-responsive').style.display = 'none';
+    document.getElementById('btn').style.display = 'block';
+    document.getElementById('btn-x').style.display = 'none';
+}
